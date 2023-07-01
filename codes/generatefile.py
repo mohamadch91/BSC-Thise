59351aece8cce987d299e29315d307cd6e89fa08
mohamadch91 <mohamadchoupan80@gmail.com>
2023-07-01 06:40:09 +0330

    feat: complete

--- codes/generatefile.py
+++ codes/generatefile.py
@@ -2,23 +2,24 @@
 #  open file
 import random
 
-f= open ("erros.txt","w")
 
-for i in range (18):
-    r=random.random()/100
-    f.write(str((i*3/100)+r))
-    f.write("\n")
-for i in range (18):
-    r=random.random()
-    f.write(str((0.3+r)))
-    f.write("\n")
-
-for i in range (18):
-    r=random.random()
-    f.write(str((0.3+r)))
-    f.write("\n")
-    
-for i in range (18):
-    r=random.random()/100
-    f.write(str((i*3/100)+r))
-    f.write("\n")  
+for j in range(1,2981):
+    f= open ("errors//"+str(j)+".txt","w")
+    for i in range (35):
+        r=random.random()/100
+        f.write(str((i*3/100)+r))
+        f.write("\n")
+    for i in range (35):
+        r=random.random()
+        f.write(str((0.1+r)))
+        f.write("\n")
+    for i in range (35):
+        r=random.random()/100
+        f.write(str((i*3/100)+r))
+        f.write("\n")  
+    for i in range (35):
+        r=random.random()
+        f.write(str((0.3+r)))
+        f.write("\n")
+        
+  
